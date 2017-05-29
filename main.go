0c0eea031a4da1f7ff30b6afab8ea50cffc4b7e1
James Weber <jpweber@gmail.com>
2017-05-29 13:59:10 -0400

    added ability to save jobs to local db and import jobs from existing db

--- main.go
+++ main.go
@@ -3,32 +3,46 @@ package main
 import (
 	"log"
 	"time"
+
+	"github.com/boltdb/bolt"
 )
 
-func checkJobs() {
-
-}
+var (
+	db *bolt.DB
+)
 
 func main() {
 
+	// Init Database
+	db = openDB("job-data.db")
+	initBucket(db, "jobs")
+
 	// Init Jobs list
 	cronJobs := Jobs{0: []Job{}}
+
+	// import any job data from db
+	importDB(db, cronJobs)
+
 	// test job data
-	testJob := Job{
-		CronPattern: "*/1 * * * *",
-		ImageName:   "jpweber/crontest:0.1.0",
-		State:       ENABLED,
-	}
-	testJob2 := Job{
-		CronPattern: "*/5 * * * *",
-		ImageName:   "jpweber/crontest2:0.1.0",
-		State:       DISABLED,
-	}
+	// testJob := Job{
+	// 	CronPattern: "*/1 * * * *",
+	// 	ImageName:   "jpweber/crontest:0.1.0",
+	// 	State:       ENABLED,
+	// }
+	// testJob.encodeHash()
+	// testJob2 := Job{
+	// 	CronPattern: "*/5 * * * *",
+	// 	ImageName:   "jpweber/crontest2:0.1.0",
+	// 	State:       DISABLED,
+	// }
+	// testJob2.encodeHash()
 
 	// add job to list of jobs
-	cronJobs.Add(testJob)
-	// cronJobs.List()
-	cronJobs.Add(testJob2)
+	// cronJobs.Add(testJob)
+	// writeToDB(db, testJob, "jobs")
+	// cronJobs.Add(testJob2)
+	// writeToDB(db, testJob2, "jobs")
+
 	cronJobs.List()
 	log.Println("Initial Jobs Loaded Starting Service")
 
